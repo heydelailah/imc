a81daa6140fbd2cd68d6e4c82ab510cc259b45cf
heydelailah <heydelailah@gmail.com>
2017-04-04 17:54:53 -0300

    up

--- peso.java
+++ peso.java
@@ -1,23 +1,30 @@
-
+//project created by alexsyfk,docent ana patricia fontes
+//created on 04/04/17 01:00pm
 import java.util.Scanner;
 class peso{
 public static void main(String args[]){
 Scanner sc = new Scanner(System.in);
-//Scanner sc2 = new Scanner(System.in);
 
 System.out.println("inforrrme quantas pessoas na residencia");
 pessoa p1 = new pessoa();
 p1.setQuant_pessoa(sc.nextInt());
 System.out.println("quantde pessoas eh "+p1.getQuant_pessoa());
+int peso[]= new int[p1.getQuant_pessoa()];
 
-/*
-int peso[]= new int[quant_pessoas];
 System.out.println("peso?");
-float 
+p1.setPeso(sc.nextFloat());
+
+
 System.out.println("altura?");
+p1.setAltura(sc.nextFloat());
+
+pessoa px[] = new pessoa[p1.getQuant_pessoa()];
+
+System.out.println("temos "+p1.getQuant_pessoa()+" pessoas na propriedade");
+System.out.println("seu peso é "+p1.getPeso());
+System.out.println("sua altura de "+p1.getAltura());
 
 
-System.out.println(peso[0]);
-*/
+
 }
 }
--- pessoa.java
+++ pessoa.java
@@ -1,8 +1,42 @@
 class pessoa{
     
   int quant_pessoa;  
-    float peso;    
+    float peso;   
+    float altura;
     float imc;
+
+
+
+public void setAltura(float x){
+    altura=x;
+    
+}
+
+
+public float getAltura(){
+    return altura;
+    
+    
+}
+
+
+
+
+
+
+
+
+public void setPeso(float x){
+    peso=x;
+    
+}
+
+
+public float getPeso(){
+    return peso;
+    
+    
+}
 
 public void setQuant_pessoa(int x){
     quant_pessoa=x;
@@ -14,6 +48,14 @@ public int getQuant_pessoa(){
     
  return quant_pessoa;
 }
+
+/*
+public void addPessoa(pessoa x){
+  for(int i=0;i<=quant_pessoa-1;i++)  
+  if()  
+}
+*/
+
 
 
 }

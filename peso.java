78e4664ffe493f2a31e02a9859868a07a2c61a78
heydelailah <heydelailah@gmail.com>
2017-04-04 16:54:10 -0300

    up

--- peso.java
+++ peso.java
@@ -1,10 +1,23 @@
+
 import java.util.Scanner;
 class peso{
 public static void main(String args[]){
 Scanner sc = new Scanner(System.in);
+//Scanner sc2 = new Scanner(System.in);
 
 System.out.println("inforrrme quantas pessoas na residencia");
-int i=sc.nextInt();
+pessoa p1 = new pessoa();
+p1.setQuant_pessoa(sc.nextInt());
+System.out.println("quantde pessoas eh "+p1.getQuant_pessoa());
 
+/*
+int peso[]= new int[quant_pessoas];
+System.out.println("peso?");
+float 
+System.out.println("altura?");
+
+
+System.out.println(peso[0]);
+*/
 }
 }
---
+++ pessoa.java
@@ -0,0 +1,19 @@
+class pessoa{
+    
+  int quant_pessoa;  
+    float peso;    
+    float imc;
+
+public void setQuant_pessoa(int x){
+    quant_pessoa=x;
+    
+}
+
+public int getQuant_pessoa(){
+    
+    
+ return quant_pessoa;
+}
+
+
+}

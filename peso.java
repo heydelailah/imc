76dcffd10c5c9af90518cc04825642a518a4d789
heydelailah <heydelailah@gmail.com>
2017-04-04 16:34:00 -0300

    up

--- peso.java
+++ peso.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
 class peso{
 public static void main(String args[]){
+Scanner sc = new Scanner(System.in);
 
-System.out.println("hello");
+System.out.println("inforrrme quantas pessoas na residencia");
+int i=sc.nextInt();
+
 }
 }
